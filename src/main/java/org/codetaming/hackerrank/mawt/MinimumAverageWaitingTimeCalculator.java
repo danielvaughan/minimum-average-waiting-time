58d6d2de3afbf46db30f7cdb77f89f3e8dfbcf9d
Daniel Vaughan <dan@danielvaughan.com>
2015-07-09 10:03:53 +0100

    calculates times but does not optimise

--- src/main/java/org/codetaming/hackerrank/mawt/MinimumAverageWaitingTimeCalculator.java
+++ src/main/java/org/codetaming/hackerrank/mawt/MinimumAverageWaitingTimeCalculator.java
@@ -1,10 +1,60 @@
 package org.codetaming.hackerrank.mawt;
 
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MinimumAverageWaitingTimeCalculator {
 
-    public int calculate(int numCustomers, List<Integer> arrivalTimes, List<Integer> preparationTimes) {
-        return 9;
+    public int calculateAverageWaitingTimes(int numCustomers, List<Integer> arrivalTimes, List<Integer> preparationTimes) {
+        double averageWaitingTime = calculateAverage(calculateWaitingTimes(numCustomers, arrivalTimes, preparationTimes));
+        return (int) averageWaitingTime;
+    }
+
+    private int lastServeTime = 0;
+
+    protected List<Integer> calculateWaitingTimes(int numCustomers, List<Integer> arrivalTimes, List<Integer> preparationTimes) {
+        lastServeTime = 0;
+        if (numCustomers != arrivalTimes.size() || numCustomers != preparationTimes.size()) {
+            throw new IllegalArgumentException("Number of arrival times and preparation times must match number of customers.");
+        }
+        List<Integer> waitingTimes = new ArrayList<>();
+        for (int customer = 0; customer < numCustomers; customer++) {
+            waitingTimes.add(calculateWaitTimeForCustomer(customer, arrivalTimes.get(customer), preparationTimes.get(customer)));
+        }
+        return waitingTimes;
+    }
+
+    private int calculateWaitTimeForCustomer(int customer, int arrivalTime, int preparationTime) {
+        int waitForCook = lastServeTime > arrivalTime ? lastServeTime - arrivalTime : 0;
+        int totalWaitTime = waitForCook + preparationTime;
+        int cookIdleTime = lastServeTime < arrivalTime ? arrivalTime - lastServeTime : 0;
+        int serveTime = lastServeTime + cookIdleTime + preparationTime;
+        outputState(customer, arrivalTime, preparationTime, waitForCook, cookIdleTime, serveTime, totalWaitTime);
+        lastServeTime = serveTime;
+        return totalWaitTime;
+    }
+
+    private void outputState(int customer, int arrivalTime, int preparationTime, int waitForCook, int cookIdleTime, int serveTime, int waitTime) {
+        System.out.println(
+                "\ncustomer: " + customer +
+                        "\narrivalTime: " + arrivalTime +
+                        "\nlastServeTime: " + lastServeTime +
+                        "\nwaitForCook: " + waitForCook +
+                        "\ncookIdleTime: " + cookIdleTime +
+                        "\npreparationTime: " + preparationTime +
+                        "\nserveTime: " + serveTime +
+                        "\nwaitTime: " + waitTime);
+    }
+
+    private double calculateAverage(List<Integer> waitingTimes) {
+        Integer sum = 0;
+        if (!waitingTimes.isEmpty()) {
+            for (Integer waitingTime : waitingTimes) {
+                sum += waitingTime;
+            }
+            return sum.doubleValue() / waitingTimes.size();
+        }
+        return sum;
     }
 }
--- src/test/java/org/codetaming/hackerrank/mawt/MinimumAverageWaitingTimeCalculatorTest.java
+++ src/test/java/org/codetaming/hackerrank/mawt/MinimumAverageWaitingTimeCalculatorTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -29,22 +28,57 @@ public class MinimumAverageWaitingTimeCalculatorTest {
         mawtc = new MinimumAverageWaitingTimeCalculator();
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void testRejectsIncorrectInput() throws Exception {
+        int numCustomers = 2;
+        List<Integer> arrivalTimes = Arrays.asList(0, 1, 2);
+        List<Integer> preparationTimes = Arrays.asList(3, 6, 9);
+        mawtc.calculateAverageWaitingTimes(numCustomers, arrivalTimes, preparationTimes);
+    }
+
+    @Test
+    public void giveOneCustomerReturnTheArrivalTimePlusPreparationTime() throws Exception {
+        assertMinimumAverageWaitingTimeIsCorrect(1, Arrays.asList(0), Arrays.asList(1), 1);
+        assertMinimumAverageWaitingTimeIsCorrect(1, Arrays.asList(0), Arrays.asList(2), 2);
+        assertMinimumAverageWaitingTimeIsCorrect(1, Arrays.asList(1), Arrays.asList(2), 2);
+    }
+
+    @Test
+    public void giveTwoCustomersReturnTheArrivalTimePlusPreparationTime() throws Exception {
+        assertMinimumAverageWaitingTimeIsCorrect(2, Arrays.asList(0, 1), Arrays.asList(1, 1), 1);
+        assertMinimumAverageWaitingTimeIsCorrect(2, Arrays.asList(0, 1), Arrays.asList(2, 1), 2);
+        assertMinimumAverageWaitingTimeIsCorrect(2, Arrays.asList(0, 1), Arrays.asList(3, 1), 3);
+        assertMinimumAverageWaitingTimeIsCorrect(2, Arrays.asList(0, 1), Arrays.asList(3, 3), 4);
+    }
+
     @Test
     public void testCalculateAcceptsCorrectSignature() throws Exception {
         int numCustomers = 3;
         List<Integer> arrivalTimes = Arrays.asList(0, 1, 2);
         List<Integer> preparationTimes = Arrays.asList(3, 6, 9);
-        Integer averageWaitTime = 0;
-        assertTrue(mawtc.calculate(numCustomers, arrivalTimes, preparationTimes) > -1);
+        mawtc.calculateAverageWaitingTimes(numCustomers, arrivalTimes, preparationTimes);
+    }
+
+    private void assertMinimumAverageWaitingTimeIsCorrect(int numCustomers, List<Integer> arrivalTimes, List<Integer> preparationTimes, Integer averageWaitTime) {
+        assertThat(mawtc.calculateAverageWaitingTimes(numCustomers, arrivalTimes, preparationTimes), is(averageWaitTime));
+    }
+
+    @Test
+    public void testCalculateWaitimesForSampleInput00() throws Exception {
+        int numCustomers = 3;
+        List<Integer> arrivalTimes = Arrays.asList(0, 1, 2);
+        List<Integer> preparationTimes = Arrays.asList(3, 9, 6);
+        List<Integer> expectedWaitTimes = Arrays.asList(3, 11, 16);
+        assertThat(mawtc.calculateWaitingTimes(numCustomers, arrivalTimes, preparationTimes), is(expectedWaitTimes));
     }
 
     @Test
     public void testCalculateWithSampleInput00() throws Exception {
         int numCustomers = 3;
         List<Integer> arrivalTimes = Arrays.asList(0, 1, 2);
-        List<Integer> preparationTimes = Arrays.asList(3, 6, 9);
+        List<Integer> preparationTimes = Arrays.asList(3, 9, 6);
         Integer averageWaitTime = 9;
-        assertThat(mawtc.calculate(numCustomers, arrivalTimes, preparationTimes), is(9));
+        assertMinimumAverageWaitingTimeIsCorrect(numCustomers, arrivalTimes, preparationTimes, averageWaitTime);
     }
 
     @Test
@@ -53,6 +87,6 @@ public class MinimumAverageWaitingTimeCalculatorTest {
         List<Integer> arrivalTimes = Arrays.asList(0, 1, 2);
         List<Integer> preparationTimes = Arrays.asList(3, 9, 5);
         Integer averageWaitTime = 9;
-        assertThat(mawtc.calculate(numCustomers, arrivalTimes, preparationTimes), is(8));
+        assertMinimumAverageWaitingTimeIsCorrect(numCustomers, arrivalTimes, preparationTimes, averageWaitTime);
     }
 }
